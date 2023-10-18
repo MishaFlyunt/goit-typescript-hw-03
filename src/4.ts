baeb523c473671a135c5f87575379ff2a427408c
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-10-18 21:16:46 +0300

    add

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,37 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected pilotInCabin = false;
+
+  public sitInPlane() {
+    this.pilotInCabin = true;
+  }
+
+  public abstract startEngine(): boolean;
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +40,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
